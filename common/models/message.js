b23d7bfafaadba978bcddf7ab0053903f577d21b
mohan <mohan@c1exchange.com>
2017-09-08 15:30:35 +0530

    login implementation

--- common/models/message.js
+++
@@ -1,8 +0,0 @@
-module.exports = function(message) {
-  message.greet = function(msg, cb) {
-    process.nextTick(function() {
-      msg = msg || 'hello';
-      cb(null, 'Sender says ' + msg + ' to receiver');
-    });
-  };
-};
