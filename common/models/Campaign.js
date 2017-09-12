d2ce52402f7f2652ffae343a915a6855fb21212c
mohan <mohan@c1exchange.com>
2017-09-12 13:09:52 +0530

    elastic stage add

--- common/models/Campaign.js
+++ common/models/Campaign.js
@@ -3,45 +3,19 @@
 module.exports = function(mediaplan) {
 
 	  
-    mediaplan.beforeRemote('find', function(ctx, instance, next) {
-		
-	if(!ctx.args.filter || !ctx.args.filter.limit) {
+  mediaplan.beforeRemote('find', function(ctx, instance, next) {
+		if(!ctx.args.filter || !ctx.args.filter.limit) {
 			 if(!ctx.args.filter) ctx.args.filter = {};
 			ctx.args.filter.limit = 10;
 		}
 		next();
 	});
 
-	mediaplan.observe('impression', function (ctx, next) {
-		console.log("mohan");
-		if (ctx.instance) {
-		  var sum = 0;
-	  
-		  mediaplan.app.models.mediaplanline.find({
-			where: {
-			  mediaplan: ctx.instance.id
-			},
-			fields: {
-			  givenNumericProperty: true
-			}
-		  }, function (err, mediaplanline) {
-			if (err) return next(err);
-	  
-			if (mediaplanline.length) {
-			  mediaplanline.forEach(function (model2) {
-				sum += mediaplanline.givenNumericProperty;
-			  });
-	  
-			  ctx.instance.calculatedProperty = sum;
-			}
-	  
-			return next();
-		  });
-	  
-		} else {
-		  return next();
-		}
-	  });
+
+
+	
+
+
 
 	
 };
---
+++ common/models/elasticsearch.js
@@ -0,0 +1,5 @@
+var elasticsearch = require('elasticsearch');
+    util = require('util'),
+    Connector = require('loopback-connector').Connector;
+
+
